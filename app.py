cb0969f4c5ce812481505aeb5fdc907c58525917
Xiaohui <xiaohui321@users.noreply.github.com>
2017-03-02 17:50:44 -0800

    Update app.py

--- app.py
+++ app.py
@@ -67,12 +67,14 @@ def processRequest(req):
     }
 
 def sendEmail(num):
+    print("test")
     sg = sendgrid.SendGridAPIClient(apikey="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")
     from_email = Email("order-from-google-assistant@vividseats.com")
     subject = "Order Confromation - Concert ticket"
     to_email = Email("zhouxiaohui93321@gmail.com")
     content = Content("text/plain", "Please confirm your purchase by clicking this link:" + num)
     mail = Mail(from_email, subject, to_email, content)
+    print("test 2")
     response = sg.client.mail.send.post(request_body=mail.get())
     print(response.status_code)
     print(response.body)
